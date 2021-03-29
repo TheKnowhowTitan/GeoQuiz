bc7c9a2f5aa5af0c65d624e13ab6e91e8ee4e361
Raghav Lohit <raghav.lohit@compass.com>
2021-03-29 22:29:44 +0530

    Trying to save the cheat status

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -4,16 +4,22 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import kotlin.properties.Delegates
 
 const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
 private const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
+private const val KEY_USER_CHEAT_STATUS = "USER_HAS_CHEATED"
 
 class CheatActivity : AppCompatActivity() {
 
     private var answerIsTrue = false
+    private var userHasCheated = false
 
     private lateinit var answerTextView: TextView
     private lateinit var showAnswerButton: Button
@@ -22,19 +28,25 @@ class CheatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
 
+        userHasCheated = savedInstanceState?.getBoolean(KEY_USER_CHEAT_STATUS, false) ?: false
+
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
         answerTextView = findViewById(R.id.answer_text_view)
         showAnswerButton = findViewById(R.id.show_answer_button)
 
         showAnswerButton.setOnClickListener {
-            val answerText = when {
-                answerIsTrue -> R.string.true_button
-                else -> R.string.false_button
-            }
-
-            answerTextView.setText(answerText)
-            setAnswerShownResult(true)
+            updateAnswerText()
+            setAnswerShownResult(userHasCheated)
         }
+    }
+
+    private fun updateAnswerText() {
+        val answerText = when {
+            answerIsTrue -> R.string.true_button
+            else -> R.string.false_button
+        }
+
+        answerTextView.setText(answerText)
     }
 
     private fun setAnswerShownResult(isAnswerShown: Boolean) {
@@ -42,6 +54,11 @@ class CheatActivity : AppCompatActivity() {
             putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
         }
         setResult(Activity.RESULT_OK, data)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putBoolean(KEY_USER_CHEAT_STATUS, userHasCheated)
     }
 
     companion object {
